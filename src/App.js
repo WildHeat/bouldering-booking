c7fe03c76bd04e76f2482379a6ae61c15bf2cba7
Aron.Berhane <lowid11+git@googlemail.com>
2023-09-27 17:00:00 +0100

    added climb page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./components/HomePage/HomePage";
 import NoPage from "./components/NoPage/NoPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ClimbPage from "./components/ClimbPage/ClimbPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/climb" element={<ClimbPage />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
       <Footer />
---
+++ src/components/ClimbPage/ClimbPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ClimbPage.css";
+
+const ClimbPage = () => {
+  return (
+    <div className="climb-page-container">
+      <div className="climb-background"></div>
+      <div className="climb-left-container"></div>
+      <div className="climb-right-container"></div>
+    </div>
+  );
+};
+
+export default ClimbPage;
