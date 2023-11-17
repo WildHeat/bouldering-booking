6a43bbebc05314f0624e9e1089667fbc6c06794c
Aron.Berhane <lowid11+git@googlemail.com>
2023-11-17 19:40:45 +0000

    fetch request for events

--- src/components/EventsPage/Event.js
+++ src/components/EventsPage/Event.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Event = ({ title, description }) => {
+const Event = ({ title, description, index }) => {
   return (
-    <div className="event">
+    <div className="event" key={index}>
       <div className="event-text-container">
         <h4 className="event-title">{title}</h4>
         <p className="event-short-description">{description}</p>
--- src/components/EventsPage/EventsPage.js
+++ src/components/EventsPage/EventsPage.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./EventsPage.css";
 import Event from "./Event";
 import Data from "./EventsData.json";
 
 const EventsPage = () => {
   const listOfEvent = Data["data"];
+
+  useEffect(() => {
+    const getAllEvents = async () => {
+      await fetch(process.env.REACT_APP_BASE_URL + "/api/v1/events/all", {
+        method: "get",
+      })
+        .then((response) => {
+          if (response.status === 403) {
+            throw response;
+          }
+          return response.json();
+        })
+        .then((body) => {
+          console.log(body);
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+    };
+    getAllEvents();
+  }, []);
 
   return (
     <div className="events-page-container">
@@ -18,9 +39,13 @@ const EventsPage = () => {
       <div className="events-content-container">
         <h1>What Events are happening?</h1>
         <div className="all-events-container">
-          {listOfEvent.map((eve) => {
+          {listOfEvent.map((eve, index) => {
             return (
-              <Event title={eve.title} description={eve.shortDescription} />
+              <Event
+                title={eve.title}
+                description={eve.shortDescription}
+                key={index}
+              />
             );
           })}
         </div>
